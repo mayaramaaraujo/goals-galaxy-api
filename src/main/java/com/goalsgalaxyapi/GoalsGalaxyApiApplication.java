1cbb85d23631999806e16682c8617884807b9c32
Mayara Araujo <mayaramaaraujo98@gmail.com>
2024-02-27 06:27:52 +0100

    initial commit

---
+++ src/main/java/com/goalsgalaxyapi/GoalsGalaxyApiApplication.java
@@ -0,0 +1,13 @@
+package com.goalsgalaxyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsGalaxyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalsGalaxyApiApplication.class, args);
+	}
+
+}
