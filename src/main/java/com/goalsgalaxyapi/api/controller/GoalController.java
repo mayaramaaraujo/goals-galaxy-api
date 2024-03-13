c2327c55ef50b92b7dcf2c39780e2ba1b3ec5783
Mayara Araujo <mayaramaaraujo98@gmail.com>
2024-03-13 06:31:37 +0100

    feat: creating controllers

--- src/main/java/com/goalsgalaxyapi/api/controller/GoalController.java
+++ src/main/java/com/goalsgalaxyapi/api/controller/GoalController.java
@@ -1,15 +1,44 @@
 package com.goalsgalaxyapi.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.goalsgalaxyapi.domain.model.Goal;
+import com.goalsgalaxyapi.domain.repository.GoalRepository;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/goals")
 public class GoalController {
 
-    @GetMapping
-    public String list() {
-        return "List goals";
+    private final GoalRepository goalRepository;
+    public GoalController(GoalRepository goalRepository) {
+        this.goalRepository = goalRepository;
     }
+
+    @GetMapping
+    public List<Goal> getAll() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Goal get(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping
+    public Goal save(@Valid @RequestBody Goal goal) {
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public Goal update(@PathVariable Long id, @RequestBody Goal goal) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+    }
+
 }
